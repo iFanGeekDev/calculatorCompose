324510bb3030fb62fb525039ec4f6ce140f2c2b9
diego <dpachecorosas96@gmail.com>
2023-12-01 08:51:48 -0800

    Calculator finish, Design by Compose

---
+++ app/src/main/java/com/example/calculatorcompose/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.calculatorcompose
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation("+")
+    object Substract : CalculatorOperation("-")
+    object Multiply : CalculatorOperation("x")
+    object Divide : CalculatorOperation("/")
+}
